fbbd4fee273b043810f8a72c6b43795bed87480a
callumzw <40447724@live.napier.ac.uk>
2021-04-07 11:50:32 +0100

    Travis Deployment

--- src/main/java/com/napier/sem/App.java
+++ src/main/java/com/napier/sem/App.java
@@ -4,7 +4,7 @@ import java.sql.*;
 import java.util.ArrayList;
 import java.util.Scanner;
 // This prints reports of Countries, Cities, Capitals and Populations...
-// pog
+//
 public class App {
     public static void main(String[] args) {
 
